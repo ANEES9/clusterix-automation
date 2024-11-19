473a9b2deb5dcec924e9d553806d41b9c8760519
b.ozturk <b.ozturk@innoscripta.com>
2024-11-19 15:00:10 +0300

    delete session storage from login tests

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,19 +1,16 @@
 import { test, expect } from '@playwright/test';
 import * as dotenv from 'dotenv';
-import * as path from 'path';
 
 const env = process.env.NODE_ENV || 'production';
 dotenv.config({ path: `.env.${env}` });
 
-test('Login to Clusterix, and save session', async ({ page }) => {
+test('Login to Clusterix', async ({ page }) => {
     await page.goto(process.env.BASE_URL || '');
     await page.getByRole('button', { name: 'Login' }).nth(1).click();
     await page.getByPlaceholder('Email').fill(process.env.EMAIL || '');
     await page.getByPlaceholder('Password').fill(process.env.PASSWORD || '');
     await page.locator('div').filter({ hasText: /^Login$/ }).first().click();
     await expect(page).toHaveURL(`${process.env.BASE_URL}`);
-    const storageFilePath = path.join('sessions', `storageState.${env}.json`);
-    await page.context().storageState({ path: storageFilePath });
 });
 
 
