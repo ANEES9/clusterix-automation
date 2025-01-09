4c30d6584446b84ff09e35a16ecbd49de395cbef
b.ozturk <b.ozturk@innoscripta.com>
2025-01-09 15:03:24 +0300

    renaming calendar page file

--- pages/calendar/calendar-page.ts
+++ pages/calendar/calendar-page.ts
@@ -1,15 +1,24 @@
 import { Page, Locator, expect } from '@playwright/test'
+import { allure } from 'allure-playwright'
 
 export class CalendarPage {
   private page: Page
   private currentApp: Locator
+
+  static readonly URL = '/calendar'
 
   constructor(page: Page) {
     this.page = page
     this.currentApp = page.locator('p.m5ZbRpDkQfW8BXDqdzmY')
   }
 
+  async goto(baseURL: string | undefined) {
+    await allure.step('Navigate to Calendar URL', async () => {
+      await this.page.goto(`${baseURL}${CalendarPage.URL}`)
+    })
+  }
   async validateCurrentApp() {
+    await expect(this.page).toHaveURL(new RegExp(`${CalendarPage.URL}$`))
     await expect(this.currentApp).toContainText('Calendar')
   }
 }
