bc2f59eb6398672a86d15ef15ee65ec7be7277e8
b.ozturk <b.ozturk@innoscripta.com>
2025-01-14 04:11:08 +0300

    files page added

---
+++ pages/files/files-page.ts
@@ -0,0 +1,24 @@
+import { Page, Locator, expect } from '@playwright/test'
+import { Allure } from 'common/allure-helper'
+
+export class EmailPage {
+  private page: Page
+  private currentApp: Locator
+
+  static readonly URL = '/cluster-space'
+
+  constructor(page: Page) {
+    this.page = page
+    this.currentApp = page.locator('p.m5ZbRpDkQfW8BXDqdzmY')
+  }
+
+  async goto(baseURL: string | undefined) {
+    await Allure.step('Navigate to Email URL', async () => {
+      await this.page.goto(`${baseURL}${EmailPage.URL}`)
+    })
+  }
+  async validateCurrentApp() {
+    await expect(this.page).toHaveURL(new RegExp(`${EmailPage.URL}$`))
+    await expect(this.currentApp).toContainText('Email')
+  }
+}
