432382628ce29a6adc91a4295cb90e90c7d8cc9a
b.ozturk <b.ozturk@innoscripta.com>
2025-01-14 22:39:03 +0300

    update product tour pop up in accounts tests

--- tests/settings/user-and-permissions/accounts.spec.ts
+++ tests/settings/user-and-permissions/accounts.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 import { Allure } from 'common/allure-helper'
 import { AccountsPage } from '../../../pages/settings/user-and-permissions/accounts-page'
 import { skipSurvey } from 'common/skip-survey'
-import { closeWelcomePopUp } from 'common/welcome-popup-helper'
+import { closeProductTour } from 'common/product-tour-helper'
 import { closeTimerPopUp } from 'common/timer-helper'
 import { addCursorStyleAndScript } from 'common/cursor-helper'
 
@@ -26,7 +26,7 @@ test.describe('User and Permissions > Accounts Tests', () => {
     })
 
     await Allure.step('Close welcome popup', async () => {
-      await closeWelcomePopUp(page)
+      await closeProductTour(page)
     })
 
     await Allure.step('Close timer popup', async () => {
