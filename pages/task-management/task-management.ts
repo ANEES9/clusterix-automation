83ee25ebf6b170ca41ec8335d03e6a7df4237f1c
shwetha.rao <raos@innoscripta.com>
2025-01-16 20:11:09 +0530

    Add Task Management page and related tests

---
+++ pages/task-management/task-management.ts
@@ -0,0 +1,154 @@
+import { Page, Locator, expect } from '@playwright/test'
+import { Allure } from 'common/allure-helper'
+import { generateRandomFileName } from 'common/randomDataGenerator'
+
+export class TaskManagementPage {
+  private page: Page
+  private taskManagement: Locator
+  private foldera: Locator
+  private addFolder: Locator
+  private renamefolder:Locator
+  private rename_fill:Locator
+  private rename_enter:Locator
+
+  private folder_click!: Locator
+  private addboard:Locator
+  private renameboard:Locator
+  private board_fill:Locator
+  private board_enter:Locator
+
+
+   
+  constructor(page: Page) {
+    this.page = page
+    this.taskManagement = page.getByRole('link', { name: 'Task Management Create and' })
+    this.foldera = page.getByRole('button', { name: 'My Pinboards' })
+    this.addFolder=page.getByText('Add folder')
+    this.renamefolder=page.locator('#scroll-area').getByRole('textbox');
+    this.rename_fill=page.locator('#scroll-area').getByRole('textbox');
+    this.rename_enter=page.locator('#scroll-area').getByRole('textbox');
+    this.addboard=page.getByText('Add pinboard');
+    this.renameboard=page.locator('#scroll-area').getByRole('textbox');
+    this.board_fill = page.locator('#scroll-area').getByRole('textbox');
+    this.board_enter=page.locator('#scroll-area').getByRole('textbox');
+  }
+ 
+
+  set saveXPath(path:string) { 
+    this.folder_click = this.page.getByText(path)
+  }
+
+  async navigateToTaskManagement() {
+    await Allure.step(
+      'should Navigate to Task Management when Task management button is clicked',
+      async () => {
+        await this.taskManagement.click()
+      }
+    )
+  }
+
+
+  async navigateToPinboardSection() {
+    await Allure.step(
+      'should navigate to the pinboard section after clicking the pinboard button',
+      async () => {
+        await this.foldera.click()
+      }
+    )
+  }
+  
+  async navigateToaddfolder() {
+    await Allure.step(
+      'should Navigate to add folder  button is clicked',
+      async () => {
+        await this.addFolder.click()
+      }
+    )
+  }
+
+
+  async navigateTorenamefolder() {
+    await Allure.step(
+      'to rename the folder',
+      async () => {
+        await this.renamefolder.press('ControlOrMeta+a')
+      }
+    )
+  }
+
+
+  async navigateTofillfolder(randomFileName:string) {
+    //const randomFileName = 'newFolderName';
+    await Allure.step(
+      'to rename the folder',
+      async () => {
+        await this.rename_fill.fill(randomFileName);
+      }
+    )
+  }
+
+
+  async renameFolderAndSubmit() {
+    await Allure.step(
+      'foldername is renamed',
+      async () => {
+        await this.rename_enter.press('Enter');
+      }
+    )
+  }
+
+
+  async clickonfolder() {
+    await Allure.step(
+      'click on folder',
+      async () => {
+        await this.folder_click.dblclick();
+      }
+    )
+  }
+
+
+  async navigateToaddpinboard() {
+    await Allure.step(
+      'should Navigate to add pinboard button is clicked',
+      async () => {
+        await this.addboard.click()
+      }
+    )
+  }
+
+
+
+  async navigateTorenameboard() {
+    await Allure.step(
+      'to rename the board',
+      async () => {
+        await this.renameboard.press('ControlOrMeta+a')
+      }
+    )
+  }
+
+
+  async navigateTofillboard(randomFileName:string) {
+    //const randomFileName = 'newFolderName';
+    await Allure.step(
+      'to rename the board',
+      async () => {
+        await this.board_fill.fill(randomFileName);
+      }
+    )
+  }
+
+
+  async renameboardAndSubmit() {
+    await Allure.step(
+      'board is renamed',
+      async () => {
+        await this.board_enter.press('Enter');
+      }
+    )
+  }
+
+}
+
+
---
+++ tests/task-management/task-board.spec.ts
@@ -0,0 +1,94 @@
+import { Page, test } from '@playwright/test'
+import { Allure } from 'common/allure-helper'
+import { ApiResponse } from 'common/api-response'
+import * as dotenv from 'dotenv'
+import { closeTimerPopUp } from 'common/timer-helper'
+import { TaskManagementPage } from 'pages/task-management'
+import { closeProductTour } from 'common/product-tour-helper'
+import { generateRandomFileName } from 'common/randomDataGenerator'
+import { skipSurvey } from 'common/skip-survey'
+import { couldStartTrivia } from 'typescript'
+
+test.describe('task management Test', () => {
+  test.beforeEach(async ({ page, baseURL }) => {
+    await Allure.step('Navigate to Base URL and Close Popups', async () => {
+      await page.goto(baseURL!)
+      await skipSurvey(page);
+      await closeProductTour(page)
+      await closeTimerPopUp(page)
+      await page.waitForLoadState('networkidle')
+ 
+    })
+  })
+
+
+  test('test case one', async ({
+    page,
+  }) => {
+      const locators = new TaskManagementPage(page)
+      
+      const taskmanageprod = 'https://task-management-backend.innoscripta.com/api/boards'
+      const taskmanagetest = 'https://task-management-backend-testing.innoscripta.com/api/boards'
+
+      const taskmanage = await ApiResponse(page, taskmanageprod, taskmanagetest)
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleDateString('en-GB'); // This will give you the format DD/MM/YYYY
+
+          // For DD.MM.YYYY format, replace the slashes with dots
+      const customFormattedDate = formattedDate.replace(/\//g, '.');
+
+      console.log(customFormattedDate);
+      const randomFileName = generateRandomFileName();
+      const board_click_name = randomFileName + customFormattedDate;
+      console.log(board_click_name);
+      console.log('Randomly Generated File Name:', randomFileName);
+      locators.saveXPath=randomFileName
+      await page.waitForLoadState('networkidle');
+      
+      //Folder creation
+      await locators.navigateToTaskManagement();
+      await locators.navigateToPinboardSection();
+      await locators.navigateToaddfolder();
+      await page.waitForTimeout(2000);
+      await locators.navigateTorenamefolder();
+      await locators.navigateTofillfolder(randomFileName);
+      await locators.renameFolderAndSubmit();
+      await page.waitForTimeout(5000);
+
+      //board creation
+
+      await locators.clickonfolder();
+      await locators.navigateToaddpinboard();
+      await page.waitForTimeout(3000);
+      await locators.navigateTorenameboard()
+      await locators.navigateTofillboard(randomFileName)
+      await locators.renameboardAndSubmit()
+      await page.waitForTimeout(5000);
+
+      
+     
+
+      const {status : apiResponseStatus } = taskmanage()
+      // Check the response and log messages accordingly
+      if (apiResponseStatus === 201) {
+          console.log('Success: API returned status 201.');
+      } else if (apiResponseStatus !== 201) {
+          console.log(`Error: API returned an unexpected status ${apiResponseStatus}.`);
+      }
+
+              // Assert that the API returned 201
+      //expect(apiResponseStatus).toBe(201);
+  })
+
+  
+
+  
+
+  
+
+  
+
+  
+})
+
+
