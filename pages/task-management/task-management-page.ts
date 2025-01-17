82db8ce819da1b34b4d5d02b8f308a641abedf66
b.ozturk <b.ozturk@innoscripta.com>
2025-01-17 04:48:22 +0300

    rename randomDataGenerator.ts

--- pages/task-management/task-management-page.ts
+++ pages/task-management/task-management-page.ts
@@ -1,40 +1,39 @@
 import { Page, Locator, expect } from '@playwright/test'
 import { Allure } from 'common/allure-helper'
-import { generateRandomFileName } from 'common/randomDataGenerator'
+import { generateRandomFileName } from 'common/random-data-generator'
 
 export class TaskManagementPage {
   private page: Page
   private taskManagement: Locator
   private foldera: Locator
   private addFolder: Locator
-  private renamefolder:Locator
-  private rename_fill:Locator
-  private rename_enter:Locator
+  private renamefolder: Locator
+  private rename_fill: Locator
+  private rename_enter: Locator
 
   private folder_click!: Locator
-  private addboard:Locator
-  private renameboard:Locator
-  private board_fill:Locator
-  private board_enter:Locator
+  private addboard: Locator
+  private renameboard: Locator
+  private board_fill: Locator
+  private board_enter: Locator
 
-
-   
   constructor(page: Page) {
     this.page = page
-    this.taskManagement = page.getByRole('link', { name: 'Task Management Create and' })
+    this.taskManagement = page.getByRole('link', {
+      name: 'Task Management Create and',
+    })
     this.foldera = page.getByRole('button', { name: 'My Pinboards' })
-    this.addFolder=page.getByText('Add folder')
-    this.renamefolder=page.locator('#scroll-area').getByRole('textbox');
-    this.rename_fill=page.locator('#scroll-area').getByRole('textbox');
-    this.rename_enter=page.locator('#scroll-area').getByRole('textbox');
-    this.addboard=page.getByText('Add pinboard');
-    this.renameboard=page.locator('#scroll-area').getByRole('textbox');
-    this.board_fill = page.locator('#scroll-area').getByRole('textbox');
-    this.board_enter=page.locator('#scroll-area').getByRole('textbox');
+    this.addFolder = page.getByText('Add folder')
+    this.renamefolder = page.locator('#scroll-area').getByRole('textbox')
+    this.rename_fill = page.locator('#scroll-area').getByRole('textbox')
+    this.rename_enter = page.locator('#scroll-area').getByRole('textbox')
+    this.addboard = page.getByText('Add pinboard')
+    this.renameboard = page.locator('#scroll-area').getByRole('textbox')
+    this.board_fill = page.locator('#scroll-area').getByRole('textbox')
+    this.board_enter = page.locator('#scroll-area').getByRole('textbox')
   }
- 
 
-  set saveXPath(path:string) { 
+  set saveXPath(path: string) {
     this.folder_click = this.page.getByText(path)
   }
 
@@ -47,7 +46,6 @@ export class TaskManagementPage {
     )
   }
 
-
   async navigateToPinboardSection() {
     await Allure.step(
       'should navigate to the pinboard section after clicking the pinboard button',
@@ -56,7 +54,7 @@ export class TaskManagementPage {
       }
     )
   }
-  
+
   async navigateToaddfolder() {
     await Allure.step(
       'should Navigate to add folder  button is clicked',
@@ -66,47 +64,30 @@ export class TaskManagementPage {
     )
   }
 
-
   async navigateTorenamefolder() {
-    await Allure.step(
-      'to rename the folder',
-      async () => {
-        await this.renamefolder.press('ControlOrMeta+a')
-      }
-    )
+    await Allure.step('to rename the folder', async () => {
+      await this.renamefolder.press('ControlOrMeta+a')
+    })
   }
 
-
-  async navigateTofillfolder(randomFileName:string) {
+  async navigateTofillfolder(randomFileName: string) {
     //const randomFileName = 'newFolderName';
-    await Allure.step(
-      'to rename the folder',
-      async () => {
-        await this.rename_fill.fill(randomFileName);
-      }
-    )
+    await Allure.step('to rename the folder', async () => {
+      await this.rename_fill.fill(randomFileName)
+    })
   }
-
 
   async renameFolderAndSubmit() {
-    await Allure.step(
-      'foldername is renamed',
-      async () => {
-        await this.rename_enter.press('Enter');
-      }
-    )
+    await Allure.step('foldername is renamed', async () => {
+      await this.rename_enter.press('Enter')
+    })
   }
-
 
   async clickonfolder() {
-    await Allure.step(
-      'click on folder',
-      async () => {
-        await this.folder_click.dblclick();
-      }
-    )
+    await Allure.step('click on folder', async () => {
+      await this.folder_click.dblclick()
+    })
   }
-
 
   async navigateToaddpinboard() {
     await Allure.step(
@@ -117,38 +98,22 @@ export class TaskManagementPage {
     )
   }
 
-
-
   async navigateTorenameboard() {
-    await Allure.step(
-      'to rename the board',
-      async () => {
-        await this.renameboard.press('ControlOrMeta+a')
-      }
-    )
+    await Allure.step('to rename the board', async () => {
+      await this.renameboard.press('ControlOrMeta+a')
+    })
   }
 
-
-  async navigateTofillboard(randomFileName:string) {
+  async navigateTofillboard(randomFileName: string) {
     //const randomFileName = 'newFolderName';
-    await Allure.step(
-      'to rename the board',
-      async () => {
-        await this.board_fill.fill(randomFileName);
-      }
-    )
+    await Allure.step('to rename the board', async () => {
+      await this.board_fill.fill(randomFileName)
+    })
   }
-
 
   async renameboardAndSubmit() {
-    await Allure.step(
-      'board is renamed',
-      async () => {
-        await this.board_enter.press('Enter');
-      }
-    )
+    await Allure.step('board is renamed', async () => {
+      await this.board_enter.press('Enter')
+    })
   }
-
 }
-
-
