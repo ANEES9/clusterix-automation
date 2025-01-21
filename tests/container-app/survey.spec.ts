ad7337f9e45821282eaf343f0339b48cef580a34
b.ozturk <b.ozturk@innoscripta.com>
2025-01-22 00:22:03 +0300

    update survey spec

--- tests/container-app/survey.spec.ts
+++ tests/container-app/survey.spec.ts
@@ -1,17 +1,17 @@
 import { test } from '@playwright/test'
-import { SurveyPage } from '../../pages/container-app/survey-page'
+import { SurveyPage } from 'pages/container-app/survey-page'
 
 test.describe('Survey Tests', () => {
+  let surveyPage: SurveyPage
+
   test.beforeEach(async ({ page, baseURL }, testInfo) => {
     const locale = testInfo.project.use.locale || 'en'
-    const surveyPage = new SurveyPage(page, locale)
+    surveyPage = new SurveyPage(page, locale)
     // Navigate to the login page and set the locale
     await surveyPage.goto(baseURL)
   })
 
-  test('Should log in with invalid credentials', async ({ page }, testInfo) => {
-    const locale = testInfo.project.use.locale
-    const surveyPage = new SurveyPage(page, locale)
+  test('Complete role question', async () => {
     await surveyPage.completeRoleQuestion('developerOption')
   })
 })
