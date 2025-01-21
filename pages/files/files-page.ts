61759b172c037f61af1cd283ee183849256a9ffb
b.ozturk <b.ozturk@innoscripta.com>
2025-01-22 02:42:39 +0300

    fix files page and tests

--- pages/files/files-page.ts
+++ pages/files/files-page.ts
@@ -1,17 +1,147 @@
-import { Page } from '@playwright/test'
+import { Locator, Page } from '@playwright/test'
 import { Allure } from 'common/allure-helper'
 import { APP_URLS } from 'config/constants/app-urls'
+import { getTranslations } from 'common/get-translations-helper'
 
-export class EmailPage {
+export class FilesPage {
   private page: Page
+  private translations: Record<string, any>
 
-  constructor(page: Page) {
+  private newButton: Locator
+  private folderButton: Locator
+  private cellButton: Locator
+  private renameButton: Locator
+  private trashButton: Locator
+  private sharedWithMeButton: Locator
+  private trashSectionButton: Locator
+  private favoritesButton: Locator
+  private themeIcon: Locator
+  private firstElement: Locator
+  private secondElement: Locator
+  private notesInput: Locator
+  private tagsSection: Locator
+  private tagInput: Locator
+  private tagByText: (tagName: string) => Locator
+  private uploadButton: Locator
+  private fileInput: Locator
+
+  constructor(page: Page, locale: string) {
     this.page = page
+    this.translations = getTranslations('files', locale)
+
+    this.newButton = this.page.getByRole('button', { name: 'New' })
+    this.folderButton = this.page.getByRole('button', { name: 'Folder' })
+    this.cellButton = this.page.getByRole('button', { name: 'Cell' })
+    this.renameButton = this.page.getByRole('button', { name: 'Rename' })
+    this.trashButton = this.page.locator(
+      'tr:nth-child(2) > td:nth-child(6) > .fTrbfOFwnQNNmUXd5tm3'
+    )
+    this.sharedWithMeButton = this.page.getByRole('button', {
+      name: 'Shared with me',
+    })
+    this.trashSectionButton = this.page.getByRole('button', { name: 'Trash' })
+    this.favoritesButton = this.page.getByRole('button', { name: 'Favorites' })
+    this.themeIcon = page.locator('.ca-fill-theme > path').first()
+    this.firstElement = page.locator('.fTrbfOFwnQNNmUXd5tm3').first()
+    this.secondElement = page.locator('.d3nSvjOkwaugosP9qnnw').first()
+    this.notesInput = page.getByPlaceholder('Enter notes here')
+    this.tagsSection = page
+      .locator('div')
+      .filter({ hasText: /^TagsEnter tags$/ })
+      .nth(1)
+    this.tagInput = page.getByRole('textbox', {
+      name: 'Enter tag and press enter',
+    })
+    this.tagByText = (tagName: string) => page.getByText(tagName)
+
+    this.uploadButton = this.page
+      .locator('div')
+      .filter({ hasText: /^Upload file$/ })
+      .nth(1)
+    this.fileInput = this.page.locator('input[type="file"]')
   }
 
   async goto(baseURL: string | undefined) {
     await Allure.step('Navigate to Email URL', async () => {
-      await this.page.goto(`${baseURL}${APP_URLS.files}`)
+      await this.page.goto(`${baseURL}${APP_URLS.files.base}`)
     })
+  }
+
+  private async waitAndClick(locator: Locator, timeout = 100000) {
+    await locator.waitFor({ timeout })
+    await locator.click()
+  }
+
+  async clickNewButton() {
+    await this.newButton.click()
+  }
+
+  async clickFolderButton() {
+    await this.folderButton.click()
+  }
+
+  async clickCellButton() {
+    await this.cellButton.click()
+  }
+
+  async renameItem(name: string) {
+    await this.renameButton.click()
+    const textBox = this.page.getByRole('textbox').nth(1)
+    await textBox.fill(name)
+    await textBox.press('Enter')
+  }
+
+  async deleteItem() {
+    await this.trashButton.click()
+  }
+
+  async navigateToSharedWithMe() {
+    await this.sharedWithMeButton.click()
+  }
+
+  async navigateToTrash() {
+    await this.trashSectionButton.click()
+  }
+
+  async navigateToFavorites() {
+    await this.favoritesButton.click()
+  }
+
+  async clickThemeIcon() {
+    await this.themeIcon.click()
+  }
+
+  async clickFirstElement() {
+    await this.firstElement.click()
+  }
+
+  async clickSecondElement() {
+    await this.secondElement.click()
+  }
+
+  async enterNotes(note: string) {
+    await this.notesInput.click()
+    await this.notesInput.fill(note)
+  }
+
+  async addTag(tagName: string) {
+    await this.tagsSection.click()
+    await this.tagInput.click()
+    await this.tagInput.fill(tagName)
+    await this.tagInput.press('Enter')
+  }
+
+  async clickUploadButton() {
+    await this.uploadButton.click()
+  }
+
+  async uploadFile(filePath: string) {
+    await this.fileInput.setInputFiles(filePath)
+  }
+
+  async verifyFileUploaded(fileName: string) {
+    const uploadedFile = this.page.locator(`text=${fileName}`)
+    await uploadedFile.waitFor({ state: 'visible' })
+    return uploadedFile.isVisible()
   }
 }
--- tests/files/homepage.spec.ts
+++ tests/files/homepage.spec.ts
@@ -1,16 +1,15 @@
-import { test, expect, Page } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { closeTimerPopUp } from 'common/timer-helper'
 import { skipSurveyHelper } from 'common/skip-survey-helper'
 import { skipProductTourHelper } from 'common/skip-product-tour-helper'
 import { addCursorStyleAndScript } from 'common/cursor-helper'
-///import {Allure} from '../../helpers/allure-helper'
-import { APP_URLS } from 'config/constants/app-urls'
-import { FilesPage } from '../../pages/file/Filespage'
+import { FilesPage } from 'pages/files/files-page'
 
 test.describe('File Reggression Suite', () => {
+  let filesPage: FilesPage
   test.beforeEach(async ({ page, baseURL }, testInfo) => {
-    // Allure report commanod
-    const filesPage = new FilesPage(page)
+    const locale: string = testInfo.project.use?.locale ?? 'en'
+    filesPage = new FilesPage(page, locale)
     console.log('Base URL:', baseURL)
     console.log('Navigating to:', `${baseURL}/cluster-space`)
     await filesPage.goto(baseURL)
@@ -23,11 +22,6 @@ test.describe('File Reggression Suite', () => {
   // Test case to click on New button
 
   test('Verify Files Section is Clickable', async ({ page }) => {
-    const filesPage = new FilesPage(page)
-
-    //await filesPage.navigateToFilesSection();
-
-    // Validate navigation
     const currentURL = page.url()
     expect(currentURL).toBe('https://clusterix.io/cluster-space')
 
@@ -46,8 +40,6 @@ test.describe('File Reggression Suite', () => {
   })
 
   test('Verify the right menu functions', async ({ page }) => {
-    const filesPage = new FilesPage(page)
-
     await filesPage.clickNewButton()
     await filesPage.clickFolderButton()
 
@@ -57,8 +49,6 @@ test.describe('File Reggression Suite', () => {
   })
 
   test('Download and Trash action', async ({ page }) => {
-    const filesPage = new FilesPage(page)
-
     const firstCheckbox = page.locator('#main-area label').nth(1)
     await firstCheckbox.click()
 
@@ -77,8 +67,6 @@ test.describe('File Reggression Suite', () => {
   })
 
   test('Page movement by clicking icons', async ({ page }) => {
-    const filesPage = new FilesPage(page)
-
     const firstLabel = page.locator('#main-area label').first()
     await firstLabel.click()
     await page.waitForTimeout(3000)
@@ -93,21 +81,18 @@ test.describe('File Reggression Suite', () => {
     await page.waitForTimeout(2000)
   })
 
-  test('should interact with notes and tags', async ({ page }) => {
-    const notesPage = new FilesPage(page)
-
+  test('should interact with notes and tags', async () => {
     // Interact with the page using POM methods
-    await notesPage.clickThemeIcon()
-    await notesPage.clickFirstElement()
-    await notesPage.clickSecondElement()
-    await notesPage.enterNotes('Testing purpose')
-    await notesPage.addTag('Test')
-    await notesPage.addTag('Test1')
+    await filesPage.clickThemeIcon()
+    await filesPage.clickFirstElement()
+    await filesPage.clickSecondElement()
+    await filesPage.enterNotes('Testing purpose')
+    await filesPage.addTag('Test')
+    await filesPage.addTag('Test1')
     //await notesPage.selectTag('test1');
   })
 
   test('Upload file button action', async ({ page }) => {
-    const filesPage = new FilesPage(page)
     // Click on the "Upload file" button
     await filesPage.clickUploadButton()
 
