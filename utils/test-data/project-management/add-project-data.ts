d6c102d08814c460c705aa1c40d70c704dc1c052
farheen.sania <farheen@innoscripta.com>
2025-01-31 15:42:38 +0530

    adding data files

---
+++ utils/test-data/project-management/add-project-data.ts
@@ -0,0 +1,19 @@
+// utils/add-project-data.ts
+export const addProjectData = {
+    shortTitleInput: "Sain Automation Test",
+    lessthanMinShortTitleInput: "SF",
+    morethanMaxShortTitleInput: "This is the new project I am creating to test the validation for Input field",
+    projectTitleInput: "This is the new project I am creating to test the validation for Input field as well as for Automation test script",
+    lessThanMinProjectTitleInput: "SF",
+    moreThanMaxProjectTitleInput: "The quick brown fox jumps over the lazy dog while the shimmering stars above illuminate the vast, endless sky, casting a gentle glow on the tranquil forest below, where creatures of all kinds roam freely in harmony with nature, undisturbed by the chaos of the modern world.",
+    projectShortTitle: "SF Automation",
+    projectTitle: "This is the new project for automation test scripts playwright",
+    projctStartDate: "01.01.2025",
+    projectEndDate: "31.01.2025",
+    // invalidSearchTitle: "Non ExistentSchema",
+    // minLength: "2",
+    // maxLength: "15",
+    // numberFieldName: "Age",
+    // minValue: "18",
+    // maxValue: "80"
+  }
