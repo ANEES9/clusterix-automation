97cd6ba3224640444765b9139c7a34509e250db5
b.ozturk <b.ozturk@innoscripta.com>
2025-02-04 16:23:29 +0300

    update survey spec

--- tests/home/survey.spec.ts
+++ tests/home/survey.spec.ts
@@ -5,10 +5,9 @@ import { setupTestContext } from 'utils/test-context'
 test.describe('Survey Tests', () => {
   let surveyPage: SurveyPage
 
-  test.beforeEach(async ({ page, baseURL }, testInfo) => {
+  test.beforeEach(async ({ page }, testInfo) => {
     const { locale } = await setupTestContext(page, testInfo)
     surveyPage = new SurveyPage(page, locale)
-    await surveyPage.goto(baseURL)
   })
 
   test('Complete role question', async () => {
